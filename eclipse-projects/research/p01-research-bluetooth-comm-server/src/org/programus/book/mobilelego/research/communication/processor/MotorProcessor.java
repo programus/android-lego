37cb11b17831ecf875eee10b036edb3c96a9c954
programus <programus@gmail.com>
2014-04-21 22:49:29 +0800

    modified report behavior

--- eclipse-projects/research/p01-research-bluetooth-comm-server/src/org/programus/book/mobilelego/research/communication/processor/MotorProcessor.java
+++ eclipse-projects/research/p01-research-bluetooth-comm-server/src/org/programus/book/mobilelego/research/communication/processor/MotorProcessor.java
@@ -1,5 +1,8 @@
 package org.programus.book.mobilelego.research.communication.processor;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
@@ -17,6 +20,9 @@ public class MotorProcessor implements RobotCmdProcessor {
 	}
 	
 	private EV3LargeRegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.B);
+	
+	private Timer timer = new Timer("Reporting Timer", true);
+	private TimerTask task = null;
 
 	@Override
 	public void process(RobotCommand cmd, Communicator communicator) {
@@ -34,12 +40,36 @@ public class MotorProcessor implements RobotCmdProcessor {
 			motor.stop();
 			break;
 		case Report:
-			PhoneMessage msg = new PhoneMessage();
-			msg.setType(PhoneMessage.Type.Motor);
-			msg.setIntValue(motor.getTachoCount());
-			communicator.sendPhoneMsg(msg);
+			if (((int) speed) == 0) {
+				this.stopReportTask();
+			} else {
+				this.startReportTask(communicator);
+			}
 			break;
 		}
 	}
-
+	
+	private void startReportTask(final Communicator communicator) {
+		if (task != null) {
+            task = new TimerTask() {
+                @Override
+                public void run() {
+                    PhoneMessage msg = new PhoneMessage();
+                    msg.setType(PhoneMessage.Type.Motor);
+                    msg.setIntValue(motor.getTachoCount());
+                    communicator.sendPhoneMsg(msg);
+                }
+            };
+            
+            timer.schedule(task, 0, 100);
+		}
+	}
+	
+	private void stopReportTask() {
+		if (task != null) {
+            task.cancel();
+            task = null;
+            timer.purge();
+		}
+	}
 }
