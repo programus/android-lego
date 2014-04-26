2b78800cc406e41571af9944e23ecc76030fdd7b
programus <programus@gmail.com>
2014-04-26 16:00:05 +0800

    Fixed bug. move enum into interface.

--- eclipse-projects/research/p01-research-bluetooth-comm-server/src/org/programus/book/mobilelego/research/communication/processor/MotorProcessor.java
+++ eclipse-projects/research/p01-research-bluetooth-comm-server/src/org/programus/book/mobilelego/research/communication/processor/MotorProcessor.java
@@ -7,18 +7,12 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
 import org.programus.book.mobilelego.research.communication.protocol.PhoneMessage;
+import org.programus.book.mobilelego.research.communication.protocol.Protocol;
 import org.programus.book.mobilelego.research.communication.protocol.RobotCommand;
 import org.programus.book.mobilelego.research.communication.util.Communicator;
 import org.programus.book.mobilelego.research.communication.util.Communicator.Processor;
 
 public class MotorProcessor implements Processor<RobotCommand, PhoneMessage> {
-	private static enum Command {
-		Forward,
-		Float,
-		Stop,
-		Report
-	}
-	
 	private EV3LargeRegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.B);
 	
 	private Timer timer = new Timer("Reporting Timer", true);
@@ -26,12 +20,16 @@ public class MotorProcessor implements Processor<RobotCommand, PhoneMessage> {
 
 	@Override
 	public void process(RobotCommand cmd, Communicator<RobotCommand, PhoneMessage> communicator) {
+		System.out.println(String.format("Process command: %s", cmd.toString()));
 		int cmdIndex = cmd.getIntValue();
 		float speed = cmd.getFloatValue();
 		motor.setSpeed(speed);
-		switch(Command.values()[cmdIndex]) {
+		switch(Protocol.MotorCommand.values()[cmdIndex]) {
 		case Forward:
 			motor.forward();
+			break;
+		case Backword:
+			motor.backward();
 			break;
 		case Float:
 			motor.flt(true);
@@ -41,23 +39,38 @@ public class MotorProcessor implements Processor<RobotCommand, PhoneMessage> {
 			break;
 		case Report:
 			if (((int) speed) == 0) {
+				System.out.println("stop report.");
 				this.stopReportTask();
 			} else {
+				System.out.println("start report.");
 				this.startReportTask(communicator);
 			}
 			break;
 		}
 	}
 	
+	private void sendReport(Communicator<RobotCommand, PhoneMessage> communicator, int value) {
+        PhoneMessage msg = new PhoneMessage();
+        msg.setType(PhoneMessage.Type.Motor);
+        msg.setIntValue(value);
+        communicator.send(msg);
+	}
+	
 	private void startReportTask(final Communicator<RobotCommand, PhoneMessage> communicator) {
-		if (task != null) {
+		final int tachoCount = motor.getTachoCount();
+		sendReport(communicator, tachoCount);
+		if (task == null) {
             task = new TimerTask() {
+            	private int oldCount = tachoCount;
                 @Override
                 public void run() {
-                    PhoneMessage msg = new PhoneMessage();
-                    msg.setType(PhoneMessage.Type.Motor);
-                    msg.setIntValue(motor.getTachoCount());
-                    communicator.send(msg);
+                	int tachoCount = motor.getTachoCount();
+                	if (tachoCount != oldCount) {
+                        System.out.printf("report: %d\n", motor.getTachoCount());
+                		sendReport(communicator, tachoCount);
+                		oldCount = tachoCount;
+                	}
+                	
                 }
             };
             
