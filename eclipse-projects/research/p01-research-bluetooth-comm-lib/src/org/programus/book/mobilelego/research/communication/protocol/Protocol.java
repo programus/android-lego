20f5687b9e5714d7b4236003f2e33df11b90cac9
programus <programus@gmail.com>
2014-04-27 22:41:53 +0800

    adding comments.

--- eclipse-projects/research/p01-research-bluetooth-comm-lib/src/org/programus/book/mobilelego/research/communication/protocol/Protocol.java
+++ eclipse-projects/research/p01-research-bluetooth-comm-lib/src/org/programus/book/mobilelego/research/communication/protocol/Protocol.java
@@ -2,5 +2,9 @@ package org.programus.book.mobilelego.research.communication.protocol;
 
 import java.io.Serializable;
 
+/**
+ * 所有协议的共同接口
+ * @author programus
+ */
 public interface Protocol extends Serializable {
 }
--- eclipse-projects/research/p01-research-bluetooth-comm-lib/src/org/programus/book/mobilelego/research/communication/util/Communicator.java
+++ eclipse-projects/research/p01-research-bluetooth-comm-lib/src/org/programus/book/mobilelego/research/communication/util/Communicator.java
@@ -13,14 +13,34 @@ import java.util.Map;
 import org.programus.book.mobilelego.research.communication.protocol.ExitSignal;
 import org.programus.book.mobilelego.research.communication.protocol.Protocol;
 
+/**
+ * 通讯员类。
+ * 通讯员负责持续监听网络，取得消息，
+ * 并将其转发给相应的操作员——{@link Processor}，
+ * 同时提供发送数据功能。
+ * @author programus
+ */
 public class Communicator {
+	/**
+	 * 操作员接口，所有操作员类必须实现此接口，
+	 * 用以操作通讯员传来的消息。
+	 * @param <T> 操作员可处理的消息协议
+	 */
 	public static interface Processor<T extends Protocol> {
 		void process(T msg, Communicator communicator);
 	}
-	private Map<String, List<Processor<? extends Protocol>>> processorMap = new HashMap<String, List<Processor<? extends Protocol>>>();
+
+	/** 存储所有操作员的Map。 */
+	private Map<String, List<Processor<? extends Protocol>>> 
+		processorMap = 
+		new HashMap<String, List<Processor<? extends Protocol>>>();
+
+	/** 读取消息用的输入流 */
 	private ObjectInputStream input;
+	/** 发送消息用的输出流 */
 	private ObjectOutputStream output;
 	
+	/** 标记接收线程是否仍活跃 */
 	private boolean available;
 	
 	public Communicator() {
@@ -41,15 +61,29 @@ public class Communicator {
 		this.startInputReadThread();
 	}
 	
-	public <P extends Protocol> void addProcessor(Class<P> type, Processor<P> processor) {
+	/**
+	 * 添加需要通讯员转发命令的操作员。
+	 * @param type 要追加的操作员可以处理的命令协议类型
+	 * @param processor 操作员对象
+	 */
+	public synchronized <P extends Protocol> void addProcessor(Class<P> type, Processor<P> processor) {
+		// 从Map中取出此协议类型对应的操作员列表
 		List<Processor<? extends Protocol>> processorList = processorMap.get(type.getName());
 		if (processorList == null) {
+			// 如果列表不存在，说明目前为止尚无此类型操作员被加入
+			// 创建列表
 			processorList = new LinkedList<Processor<? extends Protocol>>();
+			// 将列表放入Map
 			processorMap.put(type.getName(), processorList);
 		}
+		// 向列表中追加操作员
 		processorList.add(processor);
 	}
 	
+	/**
+	 * 发送消息。
+	 * @param msg 消息
+	 */
 	public void send(Protocol msg) {
 		synchronized (output) {
 			try {
