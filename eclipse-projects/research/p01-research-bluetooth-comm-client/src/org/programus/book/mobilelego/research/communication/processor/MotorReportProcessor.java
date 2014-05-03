600312208df4f71ef691db98e7fcefcf49b2b657
programus <programus@gmail.com>
2014-05-03 23:03:16 +0800

    added comments.

--- eclipse-projects/research/p01-research-bluetooth-comm-client/src/org/programus/book/mobilelego/research/communication/processor/MotorReportProcessor.java
+++ eclipse-projects/research/p01-research-bluetooth-comm-client/src/org/programus/book/mobilelego/research/communication/processor/MotorReportProcessor.java
@@ -4,13 +4,28 @@ import org.programus.book.mobilelego.research.communication.protocol.MotorReport
 import org.programus.book.mobilelego.research.communication.util.Communicator;
 import org.programus.book.mobilelego.research.communication.util.Communicator.Processor;
 
+/**
+ * 马达数据报告操作员
+ * @author programus
+ */
 public class MotorReportProcessor implements Processor<MotorReportMessage> {
+	/**
+	 * 报告回调接口
+	 */
 	public static interface ReportCallback {
+		/**
+		 * 具体现实报告的函数
+		 * @param msg 报告消息
+		 */
 		void displayReport(MotorReportMessage msg);
 	}
 	
 	private ReportCallback mCallback;
 	
+	/**
+	 * 设置回调接口对象
+	 * @param callback 回调接口对象
+	 */
 	public void setReportCallback(ReportCallback callback) {
 		this.mCallback = callback;
 	}
