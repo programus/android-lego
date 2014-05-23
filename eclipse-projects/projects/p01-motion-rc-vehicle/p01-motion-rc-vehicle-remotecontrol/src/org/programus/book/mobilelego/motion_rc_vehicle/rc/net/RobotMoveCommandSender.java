3bac4c6f6b2f023ac4481555834435bbdb6222a8
programus <programus@gmail.com>
2014-05-24 00:20:02 +0800

    added comments

--- eclipse-projects/projects/p01-motion-rc-vehicle/p01-motion-rc-vehicle-remotecontrol/src/org/programus/book/mobilelego/motion_rc_vehicle/rc/net/RobotMoveCommandSender.java
+++ eclipse-projects/projects/p01-motion-rc-vehicle/p01-motion-rc-vehicle-remotecontrol/src/org/programus/book/mobilelego/motion_rc_vehicle/rc/net/RobotMoveCommandSender.java
@@ -7,14 +7,24 @@ import java.util.concurrent.locks.ReentrantLock;
 import org.programus.book.mobilelego.motion_rc_vehicle.comm.protocol.RobotMoveCommand;
 import org.programus.book.mobilelego.motion_rc_vehicle.comm.util.Communicator;
 
+/**
+ * 机器人移动命令发送器。通过此发送器发送命令，可以保证命令发送不会过于频繁，从而防止由于机器人处理能力导致的命令积压。
+ * @author programus
+ *
+ */
 public class RobotMoveCommandSender {
+	/** 最小命令间隔时间，单位：毫秒 */
 	private static final int SEND_ITERVAL = 200;
 	
 	private Communicator mComm;
 	
+	/** 等待发送的命令 */
 	private RobotMoveCommand mWaitCommand;
+	/** 上次发送的命令 */
 	private RobotMoveCommand mPrevCommand;
+	/** 同步锁 */
 	private Lock mLock = new ReentrantLock();
+	/** 同步条件 */
 	private Condition mHasCommand = mLock.newCondition();
 	
 	public RobotMoveCommandSender() {
@@ -36,6 +46,10 @@ public class RobotMoveCommandSender {
         }
 	}
 	
+	/**
+	 * 请求发送机器人移动命令。对移动类命令，若有命令正在等待，将以此命令覆盖等待命令。停止类命令将立即发出。
+	 * @param cmd
+	 */
 	public void requestSend(RobotMoveCommand cmd) {
 		switch (cmd.getCommand()) {
 		case Forward:
@@ -50,6 +64,7 @@ public class RobotMoveCommandSender {
 	}
 	
 	private void startCommandSendThread() {
+		// 创建命令发送线程
 		Thread t = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -58,15 +73,18 @@ public class RobotMoveCommandSender {
 					try {
 						mLock.lockInterruptibly();
 						try {
+							// 没有命令等待时，线程暂停
 						    while (mWaitCommand == null) {
 						        mHasCommand.await();
 						    }
+						    // 有命令等待时，取出命令
 						    cmd = mWaitCommand;
 						    mWaitCommand = null;
 						} finally {
 							mLock.unlock();
 						}
 						if (!isDuplicatedCommand(cmd)) {
+							// 若此命令与前次命令不同，则发送命令
                             sendMoveCommand(cmd);
                             mPrevCommand = cmd;
 						}
@@ -80,6 +98,11 @@ public class RobotMoveCommandSender {
 		t.start();
 	}
 	
+	/**
+	 * 判断命令是否与前次相同
+	 * @param cmd 新的命令
+	 * @return 与前次命令相同时返回<code>true</code>
+	 */
 	private boolean isDuplicatedCommand(RobotMoveCommand cmd) {
 		boolean result = cmd == mPrevCommand;
 		if (!result) {
@@ -100,15 +123,25 @@ public class RobotMoveCommandSender {
 		return result;
 	}
 	
+	/**
+	 * 发送关闭引擎类命令，包含停止（stop）和滑行（float）。
+	 * @param cmd 关闭引擎类命令
+	 */
 	private synchronized void sendStayCommand(RobotMoveCommand cmd) {
 		if (mComm != null) {
             mComm.send(cmd);
 		}
 	}
 	
+	/**
+	 * 发送移动类命令，包括前进（forward）和后退（backward）。
+	 * @param cmd 移动类命令
+	 * @throws InterruptedException 线程被打断时抛出
+	 */
 	private synchronized void sendMoveCommand(RobotMoveCommand cmd) throws InterruptedException {
 		if (mComm != null) {
             mComm.send(cmd);
+            // 为确保机器人的处理时间，暂停一段时间
             Thread.sleep(SEND_ITERVAL);
 		}
 	}
