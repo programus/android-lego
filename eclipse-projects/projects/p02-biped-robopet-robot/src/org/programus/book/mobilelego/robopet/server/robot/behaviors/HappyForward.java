5e67f805c576deb9936568841118d0ada7461159
Programus <programus@gmail.com>
2014-07-14 21:29:12 +0800

    happy behavior

---
+++ eclipse-projects/projects/p02-biped-robopet-robot/src/org/programus/book/mobilelego/robopet/server/robot/behaviors/HappyForward.java
@@ -0,0 +1,30 @@
+package org.programus.book.mobilelego.robopet.server.robot.behaviors;
+
+import org.programus.book.mobilelego.robopet.server.robot.RobotBody;
+import org.programus.book.mobilelego.robopet.server.robot.RobotParam;
+
+/**
+ * 开心时的前行模式
+ * @author programus
+ *
+ */
+public class HappyForward extends AbstractBehavior {
+
+	@Override
+	public boolean takeControl() {
+		return this.param.getMood() == RobotParam.Mood.Angry;
+	}
+
+	@Override
+	public void move() {
+		int speed = RobotBody.Speed.RunSpeed.value;
+		this.body.forward(speed);
+		this.param.setHealthConsume(speed / 100);
+		this.param.please();
+		while (this.isControlling()) {
+			Thread.yield();
+		}
+		this.body.stop(false);
+	}
+
+}
