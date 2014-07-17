c325fd7d8cff2df72aa5d150559522b272425e04
Programus <programus@gmail.com>
2014-07-17 22:37:05 +0800

    init lib project.

---
+++ eclipse-projects/projects/p02-biped-robopet/p02-biped-robopet-lib/src/org/programus/book/mobilelego/robopet/comm/protocol/NetMessage.java
@@ -0,0 +1,10 @@
+package org.programus.book.mobilelego.robopet.comm.protocol;
+
+import java.io.Serializable;
+
+/**
+ * 所有网络消息的共同接口
+ * @author programus
+ */
+public interface NetMessage extends Serializable {
+}
