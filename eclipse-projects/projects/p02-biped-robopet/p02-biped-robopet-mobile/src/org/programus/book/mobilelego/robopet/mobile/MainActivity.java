5ffa7c4ee1f4125d2840d469a4e812fa9d1a49d4
Programus <programus@gmail.com>
2014-08-12 21:34:28 +0800

    copied bluetooth part. (not tested.)

--- eclipse-projects/projects/p02-biped-robopet/p02-biped-robopet-mobile/src/org/programus/book/mobilelego/robopet/mobile/MainActivity.java
+++ eclipse-projects/projects/p02-biped-robopet/p02-biped-robopet-mobile/src/org/programus/book/mobilelego/robopet/mobile/MainActivity.java
@@ -4,37 +4,63 @@ import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.programus.book.mobilelego.robopet.comm.protocol.ExitSignal;
 import org.programus.book.mobilelego.robopet.comm.protocol.NetMessage;
 import org.programus.book.mobilelego.robopet.comm.protocol.PetCommand;
+import org.programus.book.mobilelego.robopet.comm.util.Communicator;
+import org.programus.book.mobilelego.robopet.mobile.net.SppClient;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.text.Html;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private static final int REQUEST_ENABLE_BT = 5;
 	/** 语音识别对话框的请求代码 */
-	private final static int REQUEST_CODE = 1980;
+	private final static int REQUEST_VOICE_RECOGNITION = 1980;
 	
 	private final static String CMD_RE = "^([^0-9]+)([0-9]*)";
 	
 	private final static int DEFAULT_VALUE = 3;
 	
+	private static enum BtConnectState {
+		Disconnected,
+		Connecting,
+		Connected
+	};
+
+	private BtConnectState mBtConnectState = BtConnectState.Disconnected;
+
+	private BluetoothAdapter mBtAdapter;
+	private BluetoothDevice[] mDevices;
+
+	private SppClient mClient;
+	private Communicator mComm;
 	/** 主要组件，用以显示命令和点击开始识别语音 */
 	private TextView mMainView;
+	/** 连接前的遮盖层 */
+	private ViewGroup mCover;
 	
 	private Map<String, PetCommand.Command> mCmdTable = new HashMap<String, PetCommand.Command>();
 	
@@ -50,6 +76,8 @@ public class MainActivity extends Activity {
 		this.initData();
 		// 检查语音识别的可用性
 		this.checkVoiceRecognitionAvailability();
+		// 初始化网络客户端
+		this.initNetClient();
 	}
 	
 	private void initData() {
@@ -97,12 +125,51 @@ public class MainActivity extends Activity {
 	 */
 	private void initCtrls() {
 		this.mMainView = (TextView) this.findViewById(R.id.main_view);
+		this.mCover = (ViewGroup) this.findViewById(R.id.cover);
 		
 		// 按下按钮则弹出语音识别窗口
 		this.mMainView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				startVoiceRecognitionActivity();
+			}
+		});
+	}
+	
+	/** 初始化网络客户端 */
+	private void initNetClient() {
+		this.mClient = new SppClient();
+		this.mClient.setOnConnectedListener(new SppClient.OnConnectedListener() {
+			@Override
+			public void onFailed(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setBtConnectState(BtConnectState.Disconnected);
+                    }
+                });
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+			
+			@Override
+			public void onConnected(Communicator comm) {
+				try {
+                    mComm = comm;
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+                            setBtConnectState(BtConnectState.Connected);
+						}
+					});
+				} catch (Exception e) {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+                            setBtConnectState(BtConnectState.Disconnected);
+						}
+					});
+	                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+				}
 			}
 		});
 	}
@@ -115,7 +182,7 @@ public class MainActivity extends Activity {
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, this.getString(R.string.app_name));
 		// 使用请求代码启动Activity
-		this.startActivityForResult(intent, REQUEST_CODE);
+		this.startActivityForResult(intent, REQUEST_VOICE_RECOGNITION);
 	}
 	
 	private void processRecognitionResults(List<String> results) {
@@ -151,14 +218,139 @@ public class MainActivity extends Activity {
 		Toast.makeText(this, "Send message...", Toast.LENGTH_LONG).show();
 	}
 
+	/**
+	 * 取得蓝牙信息，并在未开启蓝牙时提示开启蓝牙。
+	 */
+	private void enableBluetooth() {
+		// 取得蓝牙适配器
+		this.mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (this.mBtAdapter == null) {
+			// 无法取得蓝牙适配器，说明设备不支持蓝牙
+			Toast.makeText(this, R.string.msg_bluetooth_not_supported, Toast.LENGTH_LONG).show();
+			this.finish();
+		}
+		// 检查蓝牙是否已经开启
+		if (!this.mBtAdapter.isEnabled()) {
+			// 如果没有开启，请求开启
+			this.requestEnableBluetooth();
+		} else {
+			// 如果已经开启，弹出对话框，让用户选择可连接设备。
+			this.selectDevice();
+		}
+	}
+	
+	/**
+	 * 弹出对话框，让用户选择可连接设备
+	 */
+	private void selectDevice() {
+		if (this.mBtAdapter != null) {
+			// 取出已配对设备
+			Set<BluetoothDevice> deviceSet = this.mBtAdapter.getBondedDevices();
+			mDevices = deviceSet.toArray(new BluetoothDevice[deviceSet.size()]);
+			this.askForDeviceSel(mDevices);
+		}
+	}
+	
+	/**
+	 * 请求用户开启蓝牙
+	 */
+	private void requestEnableBluetooth() {
+		Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+	}
+	
+	private void askForDeviceSel(final BluetoothDevice[] devices) {
+		if (devices.length > 0) {
+			CharSequence[] deviceDescriptions = new CharSequence[devices.length];
+			for (int i = 0; i < devices.length; i++) {
+				deviceDescriptions[i] = Html.fromHtml(String.format("<b>%s</b> [%s]", devices[i].getName(), devices[i].getAddress()));
+			}
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(R.string.title_select_device)
+				.setItems(deviceDescriptions, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						if (which < 0) {
+							setBtConnectState(BtConnectState.Disconnected);
+						} else {
+							connect(devices[which]);
+						}
+					}
+				});
+			AlertDialog dialog = builder.create();
+			dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					setBtConnectState(BtConnectState.Disconnected);
+				}
+			});
+			dialog.show();
+		} else {
+			Toast.makeText(this, R.string.msg_bluetooth_pair_necessary, Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	private void connect(BluetoothDevice device) {
+		if (mClient.isConnected()) {
+			mClient.close();
+		}
+		mClient.connect(device);
+	}
+	
+	private void disconnect() {
+		if (mClient.isConnected()) {
+			mClient.close();
+		}
+	}
+	
+	private void remoteFinish() {
+		if (mClient != null && mClient.isConnected() && mComm != null) {
+            mComm.send(ExitSignal.getInstance());
+		}
+	}
+	
+	private void setBtConnectState(BtConnectState state) {
+		this.mBtConnectState = state;
+		switch(state) {
+		case Connected:
+			this.mCover.setVisibility(View.GONE);
+			break;
+		case Disconnected:
+			this.mCover.setVisibility(View.VISIBLE);
+			break;
+		case Connecting:
+			enableBluetooth();
+			break;
+		}
+		this.invalidateOptionsMenu();
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-			// 当请求代码是语音识别对话框的请求代码 并且 Activity返回结果为正常时
-			// 取得识别到的文本列表
-			List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-			// 处理识别内容
-			this.processRecognitionResults(results);
+		switch (requestCode) {
+		case REQUEST_VOICE_RECOGNITION:
+		{
+			if (resultCode == RESULT_OK) {
+				// 当请求代码是语音识别对话框的请求代码 并且 Activity返回结果为正常时
+				// 取得识别到的文本列表
+				List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+				// 处理识别内容
+				this.processRecognitionResults(results);
+			}
+		}
+		break;
+		case REQUEST_ENABLE_BT:
+		{
+			if (resultCode == Activity.RESULT_OK) {
+				// 用户开启了蓝牙，提示用户选择设备进行连接
+				this.selectDevice();
+			} else {
+				// 否则提示需要蓝牙并退出程序
+				Toast.makeText(this, R.string.msg_bluetooth_is_necessary, Toast.LENGTH_LONG).show();
+				this.setBtConnectState(BtConnectState.Disconnected);
+			}
+		}
+		break;
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
@@ -171,14 +363,36 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		for (int i = 0; i < menu.size(); i++) {
+			menu.getItem(i).setVisible(i == mBtConnectState.ordinal());
 		}
-		return super.onOptionsItemSelected(item);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean result = super.onOptionsItemSelected(item);
+		switch (item.getItemId()) {
+		case R.id.bt_connect:
+			this.setBtConnectState(BtConnectState.Connecting);
+			break;
+		case R.id.bt_connecting:
+			Toast.makeText(this, "Connecting... please wait.", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.bt_disconnect:
+			remoteFinish();
+			disconnect();
+			this.setBtConnectState(BtConnectState.Disconnected);
+		}
+		this.invalidateOptionsMenu();
+		return result;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		this.remoteFinish();
+		this.disconnect();
 	}
 }
---
+++ eclipse-projects/projects/p02-biped-robopet/p02-biped-robopet-mobile/src/org/programus/book/mobilelego/robopet/mobile/net/SppClient.java
@@ -0,0 +1,101 @@
+package org.programus.book.mobilelego.robopet.mobile.net;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.programus.book.mobilelego.robopet.comm.util.Communicator;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+/**
+ * 基于SPP链接的客户端类
+ * @author programus
+ */
+public class SppClient {
+	public final static String SPP_UUID = "00001101-0000-1000-8000-00805F9B34FB";
+	/**
+	 * 当建立连接时回调用的接口
+	 */
+	public static interface OnConnectedListener {
+		/**
+		 * 正常建立连接时将调用此函数
+		 * @param comm 建立连接后创建的通讯员对象
+		 */
+		void onConnected(Communicator comm);
+		/**
+		 * 连接失败时调用此函数
+		 * @param e 失败时的例外
+		 */
+		void onFailed(Exception e);
+	}
+	
+	private BluetoothSocket mSocket;
+	private OnConnectedListener mConnectedListener;
+	private Communicator mComm = new Communicator();
+	
+	/**
+	 * 设置回调接口
+	 * @param listener 回调接口
+	 */
+	public void setOnConnectedListener(OnConnectedListener listener) {
+		this.mConnectedListener = listener;
+	}
+	
+	/**
+	 * 与指定的蓝牙设备进行SPP连接
+	 * @param device 指定的蓝牙设备
+	 */
+	public void connect(final BluetoothDevice device) {
+		// 为防止界面阻塞，在新线程中进行连接
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// 关闭现有连接（如果有的话）
+                close();
+                try {
+					mSocket = device.createRfcommSocketToServiceRecord(UUID.fromString(SPP_UUID));
+					mSocket.connect();
+					// 重设通讯员
+					mComm.reset(mSocket.getInputStream(), mSocket.getOutputStream());
+					mComm.clearProcessor(null);
+					if (mConnectedListener != null) {
+					    mConnectedListener.onConnected(mComm);
+					}
+				} catch (Exception e) {
+					if (mConnectedListener != null) {
+						mConnectedListener.onFailed(e);
+					}
+				}
+			}
+		}, "SPP Connect");
+		t.start();
+	}
+	
+	/**
+	 * 返回连接是否已建立
+	 * @return 连接已建立时返回true
+	 */
+	public boolean isConnected() {
+		return mSocket != null;
+	}
+	
+	/**
+	 * 关闭连接
+	 */
+	public void close() {
+		if (mComm.isAvailable()) {
+			mComm.close();
+		}
+		if (mSocket != null) {
+			try {
+				synchronized (mComm) {
+                    mSocket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			mSocket = null;
+		}
+	}
+}
