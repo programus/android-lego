3ea0ac34e7ad58da378d2dedfebe110cbb799848
Programus <programus@gmail.com>
2014-09-27 18:03:37 +0800

    Added TrafficSign support.

--- eclipse-projects/utilities/SignGenerator/src/org/programus/robot/signgenerator/ui/CodeDialog.java
+++ eclipse-projects/utilities/SignGenerator/src/org/programus/robot/signgenerator/ui/CodeDialog.java
@@ -22,16 +22,30 @@ public class CodeDialog extends JDialog {
 	/** 用来做对象持久化识别的常数。由于本类不打算做持久化，可以忽略。 */
 	private static final long serialVersionUID = 6777389406374017274L;
 	
-	private static final String[] TYPES = {
-		"int", "byte", "boolean"
-	};
+	private enum Type {
+		TrafficSign("TrafficSign"),
+		Int("int"),
+		Byte("byte"),
+		Boolean("boolean");
+		
+		public final String value;
+		
+		Type(String v) {
+			this.value = v;
+		}
+		
+		@Override
+		public String toString() {
+			return this.value;
+		}
+	}
 	private UI ui = UI.getInstance();
 
 	private SignData data;
 
 	private JTextArea text;
 	
-	private int type;
+	private Type type = Type.TrafficSign;
 	private int dim;
 	
 	public CodeDialog(SignData data) {
@@ -65,22 +79,13 @@ public class CodeDialog extends JDialog {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
 		panel.add(new JLabel(ui.getText("label.type")));
-		JComboBox<String> typeCb = new JComboBox<String>(TYPES);
-		panel.add(typeCb);
-		typeCb.setSelectedIndex(0);
-		typeCb.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				@SuppressWarnings("unchecked")
-				JComboBox<String> cb = (JComboBox<String>) e.getSource();
-				type = cb.getSelectedIndex();
-				resetTextArea();
-			}
-		});
 		
-		JComboBox<String> dimCb = new JComboBox<String>(new String[] {"[]", "[][]"});
+		JComboBox<Type> typeCb = new JComboBox<Type>(Type.values());
+		panel.add(typeCb);
+		
+		final JComboBox<String> dimCb = new JComboBox<String>(new String[] {"[]", "[][]"});
 		panel.add(dimCb);
-		dimCb.setSelectedIndex(0);
+		
 		dimCb.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -90,6 +95,20 @@ public class CodeDialog extends JDialog {
 				resetTextArea();
 			}
 		});
+		
+		typeCb.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				@SuppressWarnings("unchecked")
+				JComboBox<String> cb = (JComboBox<String>) e.getSource();
+				type = Type.values()[cb.getSelectedIndex()];
+				dimCb.setEnabled(type != Type.TrafficSign);
+				resetTextArea();
+			}
+		});
+		
+		dimCb.setSelectedIndex(0);
+		typeCb.setSelectedIndex(this.type.ordinal());
 		return panel;
 	}
 	
@@ -98,42 +117,73 @@ public class CodeDialog extends JDialog {
 	}
 	
 	private String generateCode() {
+		final int LF_MASK = 7;
 		String lf = System.getProperty("line.separator", "\n");
 		StringBuilder sb = new StringBuilder("new ");
-		sb.append(TYPES[type]).append("[]");
-		if (dim > 0) {
+		sb.append(type.value);
+		if (type != Type.TrafficSign) {
 			sb.append("[]");
-		}
-		sb.append(" {").append(lf);
-		int n = this.data.getEdgeLen();
-		for (int y = 0; y < n; y++) {
-			sb.append("\t");
 			if (dim > 0) {
-				sb.append("{");
+				sb.append("[]");
+			}
+			sb.append(" {");
+			sb.append(lf);
+		} else {
+			sb.append("(");
+		}
+		int n = this.data.getEdgeLen();
+		int count = 0;
+		for (int y = 0; y < n; y++) {
+			if (type != Type.TrafficSign) {
+				if (dim > 0) {
+					sb.append("{");
+				}
+				sb.append("\t");
 			}
 			for (int x = 0; x < n; x++) {
 				boolean b = this.data.isBlockBlack(x, y);
-				if (type == 2) {
-					sb.append(String.valueOf(b));
-				} else {
-					if (type == 1) {
-						sb.append("(byte)");
+				if (type == Type.TrafficSign) {
+					if (b) {
+						if ((count & LF_MASK) == 0) {
+							sb.append(lf);
+							sb.append('\t');
+						}
+						sb.append(String.format("0x%02x%02x, ", y, x));
+						count++;
 					}
-					sb.append(b ? "0x00" : "0xff");
+				} else {
+					if (type == Type.Boolean) {
+						sb.append(String.valueOf(b));
+					} else {
+						if (type == Type.Byte) {
+							sb.append("(byte)");
+						}
+						sb.append(b ? "0x00" : "0xff");
+					}
+					if (x < n - 1) {
+						sb.append(", ");
+					}
 				}
-				if (x < n - 1) {
+			}
+			if (type != Type.TrafficSign) {
+				if (dim > 0) {
+					sb.append("}");
+				}
+				if (y < n - 1) {
 					sb.append(", ");
 				}
+				sb.append(lf);
 			}
-			if (dim > 0) {
-				sb.append("}");
-			}
-			if (y < n - 1) {
-				sb.append(", ");
-			}
-			sb.append(lf);
 		}
-		sb.append("}");
+		if (type != Type.TrafficSign) {
+			sb.append("}");
+		} else {
+			if (count > 0) {
+				sb.delete(sb.lastIndexOf(","), sb.length());
+				sb.append(lf);
+			}
+			sb.append(")");
+		}
 		return sb.toString();
 	}
 	
