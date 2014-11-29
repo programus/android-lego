00a03f1da39e2004d069e11103cc9e0dfe6d1911
Programus <programus@gmail.com>
2014-11-29 23:06:01 +0800

    changed store mechanism from points to bits.

--- eclipse-projects/research/p03-research-image-recognition/src/org/programus/book/mobilelego/research/imagerecognitiontest/TrafficSign.java
+++ eclipse-projects/research/p03-research-image-recognition/src/org/programus/book/mobilelego/research/imagerecognitiontest/TrafficSign.java
@@ -1,7 +1,8 @@
 package org.programus.book.mobilelego.research.imagerecognitiontest;
 
-import java.util.Arrays;
+import java.util.BitSet;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -20,13 +21,15 @@ public class TrafficSign {
 	private final static int X_MASK = 0xff;
 	/** 纵坐标位移数，由于使用一个short类型存储坐标，前八位为纵坐标，所以位移数为8 */
 	private final static int Y_SHIFT = 8;
+	/** 前景色 */
+	private final int FG_COLOR = Color.BLACK;
+	/** 背景色 */
+	private final int BG_COLOR = Color.WHITE;
 	
-	/** 所有黑色点的坐标数据，主要用来比较和生成hash code。 */
-	private short[] data;
-	/** 所存储的点数 */
-	private int size;
-	/** 绘图用的点坐标值 */
-	private float[] points;
+	/** 所有的点数据 */
+	private BitSet data;
+	/** 绘图用位图 */
+	private Bitmap bmp;
 	
 	/** 绘图用的Paint对象 */
 	private Paint paint = new Paint();
@@ -37,15 +40,11 @@ public class TrafficSign {
 	 */
 	public TrafficSign(short... data) {
 		this();
-		this.size = Math.min(data.length, this.data.length);
-		System.arraycopy(data, 0, this.data, 0, size);
-		int i = 0;
 		for (short d : data) {
-			this.points[i++] = d & X_MASK;
-			this.points[i++] = d >> Y_SHIFT;
-			if (i > size << 1) {
-				break;
-			}
+			int x = d & X_MASK;
+			int y = d >> Y_SHIFT;
+			this.data.set(x + y * SIGN_EDGE_LEN);
+			this.bmp.setPixel(x, y, FG_COLOR);
 		}
 	}
 	
@@ -57,12 +56,15 @@ public class TrafficSign {
 	/** 重置数据 */
 	public void reset() {
 		int len = SIGN_EDGE_LEN * SIGN_EDGE_LEN;
-		if (this.data == null || this.data.length < len) {
-			this.data = new short[len];
-			this.points = new float[len << 1];
+		if (this.data == null) {
+			this.data = new BitSet(len);
+		} else {
+			this.data.clear();
 		}
-		Arrays.fill(data, (short)0xffff);
-		this.size = 0;
+		if (this.bmp == null) {
+			this.bmp = Bitmap.createBitmap(SIGN_EDGE_LEN, SIGN_EDGE_LEN, Bitmap.Config.ARGB_8888);
+		}
+		this.bmp.eraseColor(BG_COLOR);
 	}
 	
 	/**
@@ -72,13 +74,9 @@ public class TrafficSign {
 	 * @return 成功追加返回true，点数达到上限则返回false
 	 */
 	public boolean addPoint(int x, int y) {
-		boolean result = size < data.length;
-		if (result) {
-			this.points[size << 1] = x;
-			this.points[(size << 1) + 1] = y;
-			this.data[size++] = (short)((y << 8) | x);
-		}
-		return result;
+		this.data.set(x + y * SIGN_EDGE_LEN);
+		this.bmp.setPixel(x, y, FG_COLOR);
+		return true;
 	}
 	
 	/**
@@ -97,10 +95,8 @@ public class TrafficSign {
 		canvas.save();
 		// 放大绘图内容
 		canvas.scale((float)canvas.getWidth() / SIGN_EDGE_LEN, (float)canvas.getHeight() / SIGN_EDGE_LEN);
-		// 移动绘图原点以保证图片绘制位置
-		canvas.translate(.5f, .5f);
 		// 绘制所有的点，由于前面做了放大，每个点都会成为一个正方形
-		canvas.drawPoints(points, 0, size << 1, paint);
+		canvas.drawBitmap(this.bmp, 0, 0, paint);
 		// 回复画布状态
 		canvas.restore();
 		if (isOutOfDate) {
@@ -121,15 +117,7 @@ public class TrafficSign {
 		boolean result = false;
 		if (obj instanceof TrafficSign) {
 			TrafficSign sign = (TrafficSign) obj;
-			result = sign.size == this.size;
-			if (result) {
-				for (int i = 0; i < size; i++) {
-					if (sign.data[i] != this.data[i]) {
-						result = false;
-						break;
-					}
-				}
-			}
+			result = this.data.equals(sign.data);
 		}
 		
 		return result;
@@ -140,10 +128,16 @@ public class TrafficSign {
 	 */
 	@Override
 	public int hashCode() {
-		int hash = 0;
-		for (int i = 0; i < this.size; i++) {
-			hash = (hash << 5) - hash + this.data[i];
+		return this.data.hashCode();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#finalize()
+	 */
+	@Override
+	protected void finalize() throws Throwable {
+		if (this.bmp != null) {
+			this.bmp.recycle();
 		}
-		return hash;
 	}
 }
